807cc754faccaac8b54e1aff53037090b7681dc0
HarrisonMS <54726103+HarrisonMS@users.noreply.github.com>
2020-01-06 10:33:30 -0800

    re migrated seeds

--- users/users-model.js
+++ users/users-model.js
@@ -8,7 +8,7 @@ module.exports = {
 };
 
 function find() {
-	return db('users').select('id', 'username', 'password', 'department');
+	return db('users').select('id', 'username', 'password');
 }
 
 function findBy(filter) {
